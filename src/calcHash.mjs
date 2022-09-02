372b69ad90182516a891e8501608745e81a2cefc
Olena Honcharuk <gasmin07@gmail.com>
2022-09-02 14:20:13 +0300

    init: start task, read file

---
+++ src/calcHash.mjs
@@ -0,0 +1,27 @@
+// import crypto from 'crypto';
+// import fs from 'fs';
+// import path from 'path';
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+
+// export const calculateHash = async () => {
+//     // Write your code here
+//     const __dirname = dirname(fileURLToPath(import.meta.url));
+//     const filePath = path.join(__dirname, '../test_certs/idd_2019.cer');
+//     const fileToCalc = fs.readFile(filePath, 'utf8', (err, data) => {
+//         if (err || !filePath) {
+//             console.log('FS operation failed');
+//             throw new Error('FS operation failed');
+//         }
+//         console.log(data);
+//         const hash = crypto.createHash('sha256');
+//         hash.update(data);
+//         const hashValue = hash.digest('hex');
+//         console.log(data)
+//         console.log(hashValue);
+
+//     }
+//     );
+
+// };
+// calculateHash();
---
+++ src/index.js
@@ -0,0 +1,47 @@
+//const ASN1 = require('@lapo/asn1js');
+//import ASN1 from '@lapo/asn1js';
+//import Hex from '@lapo/asn1js/hex';
+const fileSelector = document.getElementById('file-selector');
+const openFile = document.getElementById('open-file');
+const cert1 = document.querySelector('.cert1');
+const cert2 = document.querySelector('.cert2');
+
+require([
+    'https://unpkg.com/@lapo/asn1js/asn1.js',
+    'https://unpkg.com/@lapo/asn1js/hex.js'
+], function (ASN1, Hex) {
+
+    cert1.innerText = ASN1.decode(Hex.decode('06032B6570')).content();
+    fileSelector.addEventListener('change', (event) => {
+        const fileList = event.target.files;
+        console.log(fileList);
+
+        const result = ASN1.decode(Hex.decode(FileList)).content();
+        console.log(result);
+        if (result.typeName() !== 'SEQUENCE') {
+            throw 'Неправильна структура конверта сертифіката (очікується SEQUENCE)';
+        }
+        // const tbsCertificate = result.sub[0];
+        // cert2.innerText = tbsCertificate;
+    });
+
+
+    // let fileHandle;
+    // openFile.addEventListener('click', async () => {
+    //     // Destructure the one-element array.
+    //     [fileHandle] = await window.showOpenFilePicker();
+    //     // Do something with the file handle.
+    //     console.log(fileHandle);
+    //     const file = await fileHandle.getFile();
+    //     const contents = await file.text();
+    //     console.log(contents);
+    //     const result = ASN1.decode(Hex.decode(contents)).content();
+    //     if (result.typeName() !== 'SEQUENCE') {
+    //         throw 'Неправильна структура конверта сертифіката (очікується SEQUENCE)';
+    //     }
+    //     const tbsCertificate = result.sub[0];
+    //     console.log(tbsCertificate);
+
+
+
+});
